698a29d21cacb8961ce309022be536af663f8a06
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2025-08-02 14:25:47 -0700

    feat: add support for CrystallizerJob

--- src/main/java/com/mystchonky/arsocultas/common/event/OccultismEvents.java
+++ src/main/java/com/mystchonky/arsocultas/common/event/OccultismEvents.java
@@ -2,6 +2,7 @@ package com.mystchonky.arsocultas.common.event;
 
 import com.hollingsworth.arsnouveau.common.block.tile.MobJarTile;
 import com.klikli_dev.occultism.common.entity.job.CrusherJob;
+import com.klikli_dev.occultism.common.entity.job.CrystallizerJob;
 import com.klikli_dev.occultism.common.entity.job.SmelterJob;
 import com.klikli_dev.occultism.common.entity.spirit.SpiritEntity;
 import com.mystchonky.arsocultas.ArsOcultas;
@@ -30,6 +31,22 @@ public class OccultismEvents {
 
     @SubscribeEvent
     public static void smelterJob(SmelterJob.SmelterJobEvent event) {
+        var entity = event.getEntity();
+        var level = event.getEntity().level();
+        if (entity instanceof SpiritEntity spirit) {
+            if (level.getBlockEntity(spirit.blockPosition()) instanceof MobJarTile jar) {
+                jar.dispatchBehavior(behavior -> {
+                    if (behavior instanceof SpiritBehaviour<? extends SpiritEntity> spiritBehaviour) {
+                        var remainder = spiritBehaviour.tryItemNearbyTransfer(jar, level, event.getResult());
+                        event.setResult(remainder);
+                    }
+                });
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void crystallizerJob(CrystallizerJob.CrystallizerJobEvent event) {
         var entity = event.getEntity();
         var level = event.getEntity().level();
         if (entity instanceof SpiritEntity spirit) {

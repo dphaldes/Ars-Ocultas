f3798725380bda680e4feadaa4073eb2390479b9
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2026-02-06 22:00:18 -0700

    fix: add loottable for sacrificial altar

--- src/main/java/com/mystchonky/arsocultas/data/DataProvider.java
+++ src/main/java/com/mystchonky/arsocultas/data/DataProvider.java
@@ -5,9 +5,14 @@ import com.mystchonky.arsocultas.data.client.BlockStateProvider;
 import com.mystchonky.arsocultas.data.client.ItemModelProvider;
 import com.mystchonky.arsocultas.data.recipe.EnchantingAppProvider;
 import com.mystchonky.arsocultas.data.recipe.ImbuementProvider;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.List;
+import java.util.Set;
 
 @EventBusSubscriber(modid = ArsOcultas.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class DataProvider {
@@ -18,11 +23,14 @@ public class DataProvider {
         var generator = event.getGenerator();
         var output = generator.getPackOutput();
         var helper = event.getExistingFileHelper();
+        var provider = event.getLookupProvider();
 
         //common
         event.createBlockAndItemTags(BlockTagsProvider::new, ItemTagsProvider::new);
         generator.addProvider(event.includeServer(), new ImbuementProvider(event.getLookupProvider(), generator));
         generator.addProvider(event.includeServer(), new EnchantingAppProvider(generator));
+        generator.addProvider(event.includeServer(), new LootTableProvider(output, Set.of(),
+                List.of(new LootTableProvider.SubProviderEntry(BlockLootProvider::new, LootContextParamSets.BLOCK)), provider));
 
         // client
         generator.addProvider(event.includeClient(), new BlockStateProvider(output, helper));
